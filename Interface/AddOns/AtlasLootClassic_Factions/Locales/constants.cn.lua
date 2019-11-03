e49711d167ee10d12f0a2bf36769ee65645b90a2
SwimmingTiger <hu60.cn@gmail.com>
2019-11-04 03:32:17 +0800

    副本掉落信息(AtlasLoot)：完善汉化

--- Interface/AddOns/AtlasLootClassic_Factions/Locales/constants.cn.lua
+++ Interface/AddOns/AtlasLootClassic_Factions/Locales/constants.cn.lua
@@ -3,3 +3,5 @@
 if not AL then return end
 
 -- These localization strings are translated on Curseforge: https://www.curseforge.com/wow/addons/atlaslootclassic/localization
+-- Factions
+AL["Secondary factions"] = "第二阵营"
