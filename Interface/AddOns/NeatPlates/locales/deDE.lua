1fd63a932b70d288422db5b145ef1c5bd0dabb2a
SwimmingTiger <hu60.cn@gmail.com>
2020-02-20 18:13:51 +0800

    NeatPlates 姓名板美化：更新到 1.13.3.13

---
+++ Interface/AddOns/NeatPlates/locales/deDE.lua
@@ -0,0 +1,5 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("NeatPlates", "deDE")
+if not L then return end
+
+
+L = L or {}
---
+++ Interface/AddOns/NeatPlates/locales/ruRU.lua
@@ -0,0 +1,9 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("NeatPlates", "ruRU")
+if not L then return end
+
+
+L = L or {}
+L["Are you sure you wish to delete the profile '%1'?"] = "Вы уверенны в том, что хотите удалить этот профиль?"
+L["Arrow(Left)"] = "Стрелочка (налево)"
+L["Border Color"] = "Цвет границы"
+L["Clear Spell Database"] = "Очистить базу данных заклинаний"
