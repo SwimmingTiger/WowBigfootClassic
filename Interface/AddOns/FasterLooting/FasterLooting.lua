aeab793cc23e3a1fbcf707657c9f5be95e72382f
SwimmingTiger <hu60.cn@gmail.com>
2021-05-14 23:00:59 +0800

    添加自动拾取加速

---
+++ Interface/AddOns/FasterLooting/FasterLooting.lua
@@ -0,0 +1,22 @@
+-- Time delay
+local delay = 0
+local DEBOUNCE_INTERVAL = 0.3
+
+
+-- Fast loot function
+function FastLoot()
+    if GetTime() - delay >= DEBOUNCE_INTERVAL then
+        delay = GetTime()
+        if GetCVarBool("autoLootDefault") ~= IsModifiedClick("AUTOLOOTTOGGLE") then
+            for i = GetNumLootItems(), 1, -1 do
+                LootSlot(i)
+            end
+            delay = GetTime()
+        end
+    end
+end
+
+-- event frame
+local faster = CreateFrame("Frame")
+faster:RegisterEvent("LOOT_READY")
+faster:SetScript("OnEvent", FastLoot)
