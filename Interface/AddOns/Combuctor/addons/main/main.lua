8319a2de20d60192d6d43ddd58752d1c3eeb4a35
SwimmingTiger <hu60.cn@gmail.com>
2021-07-07 03:11:12 +0800

    解决战斗中无法点[X]关闭背包的问题

--- Interface/AddOns/Combuctor/addons/main/main.lua
+++ Interface/AddOns/Combuctor/addons/main/main.lua
@@ -10,3 +10,10 @@ Addon.canSearch = true
 Addon.FrameScale = 1.0
 Addon.ItemScale = 1.0
 Addon.Slash = 'cbt'
+
+-- 老虎会游泳：解决战斗中无法点[X]关闭背包的问题
+hooksecurefunc("HideUIPanel", function(ui)
+    if InCombatLockdown() and type(ui) == 'table' and ui.bagToggle then
+        ui:Hide()
+    end
+end)
