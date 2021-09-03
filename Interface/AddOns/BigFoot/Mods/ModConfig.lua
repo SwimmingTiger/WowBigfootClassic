91e47e5110d6204c9da5b64319d24a9270bdeb84
SwimmingTiger <hu60.cn@gmail.com>
2021-09-04 02:01:47 +0800

    大脚设置接管了装备属性插件MerInspect的加载，所以小地图按钮不再显示“装备属性”复选框

--- Interface/AddOns/BigFoot/Mods/ModConfig.lua
+++ Interface/AddOns/BigFoot/Mods/ModConfig.lua
@@ -752,10 +752,10 @@ local function __AddBottomFrames()
 		end
 	end)
 
-	if IsConfigurableAddOn('MerInspect') then
+	--[[if IsConfigurableAddOn('MerInspect') then
 		check = __CreateEnableAddonCheckBox('MerInspect', nil, true, true)
 		M:AddBottomButton(check)
-	end
+	end]]
 
 	if pcall(GetCVarDefault, "targetnearestuseold") then
 	    check = __CreateOldTabCheckBox()
